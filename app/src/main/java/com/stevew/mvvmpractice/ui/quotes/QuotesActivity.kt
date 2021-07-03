aed34b7e26ffe009d2630fbd01cee33f12231a35
grizz03 <sawilson1110@yahoo.com>
2021-07-03 19:48:01 -0400

    initial commit

---
+++ app/src/main/java/com/stevew/mvvmpractice/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,12 @@
+package com.stevew.mvvmpractice.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stevew.mvvmpractice.R
+
+class QuotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
